9fe96283290ad75c11c5b5ee7d9de5695e9298b7
tyc45 <tyc450@gmail.com>
2021-10-05 16:00:27 +0200

    Created boilerplate

---
+++ 08-operators/4-fix-prompt/script.js
@@ -0,0 +1,4 @@
+/*  Votre code Javascript vient ici:
+      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
+      - N'oubliez pas de sauvegarder pour afficher les changements.
+*/
