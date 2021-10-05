ac78aef07e844a243c5804611dabb74d91578cdb
tyc45 <tyc450@gmail.com>
2021-10-05 16:34:56 +0200

    Renamed folders for their order to make sense

--- script.js
+++
@@ -1,4 +0,0 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
