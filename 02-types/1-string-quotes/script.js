d8afc63c78c7a6c1d2b7f01a5e93b1e8067b7ccf
tyc45 <tyc450@gmail.com>
2021-10-13 13:44:54 +0200

    Types and alert-prompt-confirm done

--- 02-types/1-string-quotes/script.js
+++ 02-types/1-string-quotes/script.js
@@ -1,4 +1,7 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+let name = "Ilya";
+
+alert( `hello ${1}` ); // hello 1
+
+alert( `hello ${"name"}` ); // hello name
+
+alert( `hello ${name}` ); // hello Ilya
--- 03-alert-prompt-confirm/1-simple-page/script.js
+++ 03-alert-prompt-confirm/1-simple-page/script.js
@@ -1,4 +1,2 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+let name = prompt("Quel est votre nom?");
+alert(name);
